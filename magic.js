f5eca8b1e55cdc19d1d2193a4bf3f6d7bc77122c
Bernadette Angelice Novena Octaviani <belajarcodingangelicenovenaoct@gmail.com>
2025-12-26 00:36:07 +0700

    Create magic.js

---
+++ magic.js
@@ -0,0 +1,16 @@
+// A simple script to give you daily motivation!
+const fortunes = [
+    "Believe in yourself, Angel! ",
+    "Code is like magic, you are the magician.",
+    "Your ThinkPad is ready for great things today!",
+    "Rank S is just a few commits away! ",
+    "Keep learning, keep growing. You're doing great!"
+];
+
+const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+
+console.log("====================================");
+console.log(" JS MAGIC TOOL - DAILY QUOTE ");
+console.log("====================================");
+console.log(randomFortune);
+console.log("====================================");
